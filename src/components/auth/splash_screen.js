caadf5902aa5ac427adcdbd37af64f66d571ea88
jmoseka <jamylam3@gmail.com>
2022-09-20 18:26:49 +0300

    Add splashscreen title

---
+++ src/components/auth/splash_screen.js
@@ -0,0 +1,12 @@
+function Splashscreen() {
+    return (
+        <div className="container-splash d-flex flex-column align-items-center">
+            <div className='splash-title container d-flex flex-column align-items-center'>
+                <p className='display-3'><span className='logo-text-style'>Find a doctor</span> and book online</p>
+            </div>
+            
+        </div>
+    )
+}
+
+export default Splashscreen;
