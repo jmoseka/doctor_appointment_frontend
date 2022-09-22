8f6f24fb2bda968209a7efbfd7be32f516f9e85f
jmoseka <jamylam3@gmail.com>
2022-09-22 21:28:37 +0300

    Add myappointment reducer to store

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,10 +3,12 @@ import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 import { SignUpReducer } from './auth/register';
 import { LoginUserReducer } from './auth/login';
+import myAppointmentReducer from './my_appointment_redux/my_appointment';
 
 const reducer = combineReducers({
   user: SignUpReducer,
   login: LoginUserReducer,
+  my_appointment: myAppointmentReducer,
 });
 
 const store = createStore(
