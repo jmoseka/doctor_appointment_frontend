b6e7f4bf6d64edd417337e7e8d55b52e05401c50
mandela.t <manderat206@gmail.com>
2022-09-22 13:29:18 +0200

    Add reducer

--- src/redux/doctorReduce/doctors.js
+++ src/redux/doctorReduce/doctors.js
@@ -6,7 +6,7 @@ const GET_DOCTORS = 'DOCTOR_APPOINTMENT/GET_DOCTORS';
 const CREATE_DOCTOR = 'DOCTOR_APPOINTMENT/CREATE_DOCTOR';
 const DELETE_DOCTOR = 'DOCTOR_APPOINTMENT/DELETE_DOCTOR';
 
-const doctorsState = []
+const doctorsState = [];
 
 const getDoctors = (payload) => ({
   type: GET_DOCTORS,
@@ -44,6 +44,20 @@ export const deleteDoctorAction = (payload) => async (dispatch) => {
     .delete(`http://localhost:3000/doctors/${payload}`)
     .then((res) => {
       dispatch(deleteDoctor(res.data));
-    }); 
+    });
 };
 
+const doctorsReducer = (state = doctorsState, action) => {
+  switch (action.type) {
+    case GET_DOCTORS:
+      return action.payload;
+    case CREATE_DOCTOR:
+      return [...state, action.payload];
+    case DELETE_DOCTOR:
+      return state.filter((doctor) => doctor.id !== action.payload.id);
+    default:
+      return state;
+  }
+};
+
+export default doctorsReducer;
