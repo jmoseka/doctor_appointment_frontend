193960349c443169379415d766d2a109de208af1
mandela.t <manderat206@gmail.com>
2022-09-22 13:21:26 +0200

    Add action type

--- src/redux/doctorReduce/doctors.js
+++ src/redux/doctorReduce/doctors.js
@@ -4,3 +4,19 @@ const GET_DOCTORS = 'DOCTOR_APPOINTMENT/GET_DOCTORS';
 const CREATE_DOCTOR = 'DOCTOR_APPOINTMENT/CREATE_DOCTOR';
 const DELETE_DOCTOR = 'DOCTOR_APPOINTMENT/DELETE_DOCTOR';
 
+const doctorsState = []
+
+const getDoctors = (payload) => ({
+  type: GET_DOCTORS,
+  payload,
+});
+
+const createDoctor = (payload) => ({
+  type: CREATE_DOCTOR,
+  payload,
+});
+
+const deleteDoctor = (payload) => ({
+  type: DELETE_DOCTOR,
+  payload,
+});
