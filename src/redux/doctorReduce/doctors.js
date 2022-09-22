2b34e448a004c182f1397be588397b9e693587c8
mandela.t <manderat206@gmail.com>
2022-09-22 13:27:54 +0200

    Add action type

--- src/redux/doctorReduce/doctors.js
+++ src/redux/doctorReduce/doctors.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
+// const BASE_URL = 'http://127.0.0.1:3000/api/v1';
+
 const GET_DOCTORS = 'DOCTOR_APPOINTMENT/GET_DOCTORS';
 const CREATE_DOCTOR = 'DOCTOR_APPOINTMENT/CREATE_DOCTOR';
 const DELETE_DOCTOR = 'DOCTOR_APPOINTMENT/DELETE_DOCTOR';
@@ -20,3 +22,28 @@ const deleteDoctor = (payload) => ({
   type: DELETE_DOCTOR,
   payload,
 });
+
+export const getDoctorsAction = () => async (dispatch) => {
+  axios
+    .get('http://localhost:3000/doctors')
+    .then((res) => {
+      dispatch(getDoctors(res.data));
+    });
+};
+
+export const createDoctorAction = (payload) => async (dispatch) => {
+  axios
+    .post('http://localhost:3000/doctors', payload)
+    .then((res) => {
+      dispatch(createDoctor(res.data));
+    });
+};
+
+export const deleteDoctorAction = (payload) => async (dispatch) => {
+  axios
+    .delete(`http://localhost:3000/doctors/${payload}`)
+    .then((res) => {
+      dispatch(deleteDoctor(res.data));
+    }); 
+};
+
