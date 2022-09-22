0de9f39135195d9aa76a0ef6f9b83f84b2ee7746
jmoseka <jamylam3@gmail.com>
2022-09-22 21:03:30 +0300

    Add my appointment reducer

--- src/App.js
+++ src/App.js
@@ -1,18 +1,39 @@
 import { Route, Routes } from 'react-router-dom';
 import AddDoctor from './components/addDoctor/AddDoctor';
-import Appointments from './components/my_appointment/Appointments';
 // import Signup from './components/auth/signup';
 // import Splashscreen from './components/auth/splash_screen';
 // import Signin from './components/auth/signin';
 import DeleteDoctor from './components/DeleteDoctor';
 import DoctorDetails from './components/DoctorDetails';
 import Doctors from './components/Doctors';
+import Appointments from './components/my_appointment/Appointments';
 import Navbar from './components/Navbar';
 import NewAppointment from './components/new-appointment/NewAppointment';
 import NotFound from './components/NotFound';
 // import localStorages from './helpers/localStorage';
 
 const App = () => (
+// const hasAccount = localStorages.getUser();
+
+  // if (hasAccount.user) {
+  //   return (
+  //     <div>
+  //       <Navbar />
+  //       <section id="main-sec">
+  //         <Routes>
+  //           <Route path="/" element={<Doctors />} />
+  //           <Route path="/add_doctor" element={<AddDoctor />} />
+  //           <Route path="/appointments" element={<Appointments />} />
+  //           <Route path="/new_appointment" element={<NewAppointment />} />
+  //           <Route path="/delete_doctor" element={<DeleteDoctor />} />
+  //           <Route path="/doctor_details/:id" element={<DoctorDetails />} />
+  //           <Route path="/*" element={<NotFound />} />
+  //         </Routes>
+  //       </section>
+  //     </div>
+  //   );
+  // }
+
   <div>
     <Navbar />
     <section id="main-sec">
@@ -26,7 +47,15 @@ const App = () => (
         <Route path="/*" element={<NotFound />} />
       </Routes>
     </section>
-  </div>
-);
 
+    {/* <Splashscreen />
+    <Routes>
+      <Route path="/splashscreen" element={<Splashscreen />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/signin" element={<Signin />} />
+    </Routes> */}
+
+  </div>
+
+);
 export default App;
--- src/redux/my_appointment_redux/my_appointment.js
+++ src/redux/my_appointment_redux/my_appointment.js
@@ -1,7 +1,25 @@
 import axios from 'axios';
+
 const GET_DATA = 'doctor_appointment_frontend/my_appointment/getData';
 
 export const getData = (payload) => ({
   type: GET_DATA,
   payload,
 });
+
+export const fetchData = () => async () => {
+  const url = 'http://localhost:3000/api/v1/users';
+  const response = await axios.get(url);
+  // const data = await response.data;
+  // const myAppointmentData = [];
+  console.log(response);
+};
+
+export default function myAppointmentReducer(state = [], action) {
+  switch (action.type) {
+    case GET_DATA:
+      return action.payload;
+    default:
+      return state;
+  }
+}
