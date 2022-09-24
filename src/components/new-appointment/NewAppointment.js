401b5db2ab731fa61f081601d2d6a716b685f58a
mandela.t <manderat206@gmail.com>
2022-09-24 14:34:35 +0200

    Add new appointment to the api

--- src/components/new-appointment/NewAppointment.js
+++ src/components/new-appointment/NewAppointment.js
@@ -1,51 +1,103 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './new_Appointment.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { reserveAction } from '../../redux/my_appointment_redux/my_appointment';
+import { getDoctorsAction } from '../../redux/doctorReduce/doctors';
 
-const NewAppointment = () => (
-  <div className=" reserveContainer ">
-    <div>
-      <div className="flex">
-        <h4 className="reserveFormHeading">Book Appointment</h4>
-      </div>
+const NewAppointment = () => {
+  const dispatch = useDispatch();
+  // const reserveDoctor = useSelector((state) => state.my_appointment);
+  const doctors = useSelector((state) => state.doctor);
+  const userId = JSON.parse(localStorage.getItem('user'));
 
-      <div className="resrve-header">
-        <p className="text-doctors">
-          We have different doctors from different parts of the world.
-          <br />
-          If you wish to find out if we have a doctor who is available in your
-          city, please use the selector below
-        </p>
-      </div>
+  const [doctorId, setDoctorId] = useState('');
+  const [city, setCity] = useState('');
+  const [dateReserved, setDateReserved] = useState('');
 
-      <form action="/#" className="form-list">
-        <div className="details-form">
-          <input type="text" value="Name" className="form-user-name" />
-          <select name="cities" id="cities" className="form-user-name">
-            <option value="Nairobi">Nairobi</option>
-            <option value="Delhi">Accra</option>
-            <option value="Abuja">Kigali</option>
-            <option value="Lusaka">Lusaka</option>
-          </select>
-          <select
-            name="availableDoctors"
-            id="availableDoctors"
-            className="form-user-name"
-          >
-            <option value="Ranjeet"> Dr.Lion Rouge </option>
-            <option value="Ranjeet"> Dr.Asiati Magana </option>
-            <option value="Ranjeet"> Dr.Jamila Moseka </option>
-            <option value="Ranjeet"> Dr.Eric Mahale </option>
-            <option value="Ranjeet"> Dr.Mandela </option>
-          </select>
-          <input type="date" value="Date" className="form-user-name" />
-          <br />
+  useEffect(() => {
+    dispatch(getDoctorsAction());
+  }, [dispatch]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const data = {
+      user_id: userId.user.id,
+      city,
+      date_reserved: dateReserved,
+      doctor_id: doctorId,
+    };
+    dispatch(reserveAction(data));
+  };
+
+  console.log(userId.user.id);
+
+  return (
+    <div className=" reserveContainer ">
+      <div>
+        <div className="flex">
+          <h4 className="reserveFormHeading">Book Appointment</h4>
         </div>
-        <div className="form-book">
-          <input type="submit" value="Book Now" className="form-user-button" />
+
+        <div className="resrve-header">
+          <p className="text-doctors">
+            We have different doctors from different parts of the world.
+            <br />
+            If you wish to find out if we have a doctor who is available in your
+            city, please use the selector below
+          </p>
         </div>
-      </form>
+
+        <form action="/#" className="form-list" onSubmit={handleSubmit}>
+          <div className="details-form">
+            <div className="form-group">
+              <p className="ms-4 mb-1">City</p>
+              <input
+                type="text"
+                name="city"
+                id="city-town"
+                className="form-control rounded-pill"
+                placeholder="Enter your city"
+                required
+                onChange={(e) => setCity(e.target.value)}
+              />
+            </div>
+            <select
+              name="availableDoctors"
+              id="availableDoctors"
+              value={doctorId}
+              className="form-user-name"
+              onChange={(e) => setDoctorId(e.target.value)}
+            >
+              {doctors.map((doctor) => (
+                <option value={doctor.id} key={doctor.id}>
+                  {doctor.name}
+                </option>
+              ))}
+              {/* <option value="Ranjeet"> Dr.Lion Rouge </option>
+              <option value="Ranjeet"> Dr.Asiati Magana </option>
+              <option value="Ranjeet"> Dr.Jamila Moseka </option>
+              <option value="Ranjeet"> Dr.Eric Mahale </option>
+              <option value="Ranjeet"> Dr.Mandela </option> */}
+            </select>
+            <p className="ms-4 mb-1">Date</p>
+            <input
+              type="date"
+              value={dateReserved}
+              className="form-user-name"
+              onChange={(e) => setDateReserved(e.target.value)}
+            />
+            <br />
+          </div>
+          <div className="form-book">
+            <input
+              type="submit"
+              value="Book Now"
+              className="form-user-button"
+            />
+          </div>
+        </form>
+      </div>
     </div>
-  </div>
-);
-
+  );
+};
 export default NewAppointment;
