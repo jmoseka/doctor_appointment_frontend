215d33cbd84f24e0761e1632487048c3b1890bd5
mandela.t <manderat206@gmail.com>
2022-09-24 15:50:06 +0200

    Modifie

--- src/components/auth/signin.js
+++ src/components/auth/signin.js
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
+import toast from 'react-hot-toast';
 import { Link } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { LoginUserAction } from '../../redux/auth/login';
 import './css/sign_in_up.css';
 
 function Signin() {
+  const notify = () => toast('Logged in successfully.');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
@@ -26,16 +28,13 @@ function Signin() {
           <div className="line-hor d-flex justify-content-center">
             <span className="line " />
           </div>
-
         </div>
         <div className="logo-text hidden mt-5">
           <p className="p-2 logo-content">
-            The leading online booking website
-            for doctors anywhere, anytime. Take care of your body it will take care of you
-
+            The leading online booking website for doctors anywhere, anytime.
+            Take care of your body it will take care of you
           </p>
         </div>
-
       </section>
       <section className="section-form d-flex flex-column align-items-center mt-4 mt-md-5 pt-md-5">
         <div className="form-title">
@@ -46,15 +45,37 @@ function Signin() {
           <form className="signup-form" onSubmit={handleSubmit}>
             <div className="form-group">
               <p className="ms-4 mb-1">Email</p>
-              <input type="text" name="email" id="signup-email" className="form-control rounded-pill" placeholder="Enter your email" required onChange={(e) => setEmail(e.target.value)} />
+              <input
+                type="text"
+                name="email"
+                id="signup-email"
+                className="form-control rounded-pill"
+                placeholder="Enter your email"
+                required
+                onChange={(e) => setEmail(e.target.value)}
+              />
             </div>
             <div className="form-group">
               <p className="ms-4 mb-1">Password</p>
-              <input type="password" name="password" id="signup-password" className="form-control rounded-pill" placeholder="Enter your password" required onChange={(e) => setPassword(e.target.value)} />
+              <input
+                type="password"
+                name="password"
+                id="signup-password"
+                className="form-control rounded-pill"
+                placeholder="Enter your password"
+                required
+                onChange={(e) => setPassword(e.target.value)}
+              />
             </div>
 
             <div className="signup-buttons mt-5 text-center">
-              <button type="submit" className="style-btn rounded-pill" to="/">LOGIN</button>
+              <button
+                type="submit"
+                className="style-btn rounded-pill"
+                onClick={notify}
+              >
+                LOGIN
+              </button>
             </div>
           </form>
 
@@ -62,7 +83,11 @@ function Signin() {
             <p>
               Dont have an account?
               {' '}
-              <span className="login-link"><Link to="/signup" id="login-on-signup">Sign up</Link></span>
+              <span className="login-link">
+                <Link to="/signup" id="login-on-signup">
+                  Sign up
+                </Link>
+              </span>
             </p>
           </div>
         </div>
