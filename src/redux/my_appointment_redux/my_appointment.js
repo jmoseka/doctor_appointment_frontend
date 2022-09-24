357077994a4e4d85b3339aafc489392c3f7bdbe4
mandela.t <manderat206@gmail.com>
2022-09-24 14:35:14 +0200

    Add notification

--- src/redux/my_appointment_redux/my_appointment.js
+++ src/redux/my_appointment_redux/my_appointment.js
@@ -1,19 +1,25 @@
 /* eslint-disable camelcase */
-import axios from 'axios';
+import { NotificationManager } from 'react-notifications';
+import axios from '../../config/axios';
 
 const GET_DATA = 'doctor_appointment_frontend/my_appointment/getData';
+const CREATE_RESERVE = 'doctor_appointment_frontend/my_appointment/RESERVE';
 
 export const getData = (payload) => ({
   type: GET_DATA,
   payload,
 });
 
+export const createReserve = (payload) => ({
+  type: CREATE_RESERVE,
+  payload,
+});
+
 export const fetchData = () => async (dispatch) => {
-  const url = 'http://127.0.0.1:3000/api/v1/users/3/reservations';
+  const url = '/api/v1/users/3/reservations';
   const response = await axios.get(url);
   const data = await response.data;
   const arr = [];
-  // console.log(typeof (data));
   data.forEach((element) => {
     const { city, date_reserved } = element;
     const { name, id } = element.doctor;
@@ -27,10 +33,29 @@ export const fetchData = () => async (dispatch) => {
   dispatch(getData(arr));
 };
 
+export const reserveAction = (payload) => async (dispatch) => {
+  axios
+    .post(`/api/v1/users/${payload.user_id}/reservations`, payload)
+    .then((res) => {
+      dispatch(createReserve(res.data));
+      NotificationManager.success(
+        'You have reserved a doctor!',
+        'Successful!',
+        2000,
+      );
+    })
+    .catch((error) => {
+      dispatch(createReserve(error.response.data));
+      NotificationManager.error('Error while creating new book!', 'Error!');
+    });
+};
+
 export default function myAppointmentReducer(state = [], action) {
   switch (action.type) {
     case GET_DATA:
       return action.payload;
+    case CREATE_RESERVE:
+      return [...state, action.payload];
     default:
       return state;
   }
