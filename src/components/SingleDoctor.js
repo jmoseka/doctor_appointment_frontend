8f76c65356cb62eec7c49a02961939336c795271
Eric Mahare <erikmahare@gmail.com>
2022-09-26 11:46:24 +0300

    Removed speciality from the doctors details cauresel

--- src/components/SingleDoctor.js
+++ src/components/SingleDoctor.js
@@ -1,4 +1,3 @@
-import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import insta from '../images2/insta-icon-home.png';
 import fb from '../images2/fb-icon-home.png';
@@ -11,38 +10,33 @@ const social = [
 ];
 
 const SingleDoctor = ({
-  id, name, image, speciality, location, description,
+  name, image, location, description,
 }) => (
-  <Link to={`/doctor_details/${id}`} id="RouterNavLink">
-    <div className="doctors1">
-      <div className="image-div">
-        <img src={image} alt="Profile of doctor" className="doctors-images" />
-      </div>
-      <div className="doctor-data">
-        <h4>{name}</h4>
-        <p>.........................</p>
-        <p>{speciality}</p>
-        <p>
-          {description}
-        </p>
-        <p>{location}</p>
-        <div className="social-net">
-          {social.map((item) => (
-            <a href="/" key={item.icon}>
-              <img src={item.icon} alt="social" className="w-8" />
-            </a>
-          ))}
-        </div>
+  <div className="doctors1">
+    <div className="image-div">
+      <img src={image} alt="Profile of doctor" className="doctors-images" />
+    </div>
+    <div className="doctor-data">
+      <h4>{name}</h4>
+      <p>.........................</p>
+      <p>
+        {description}
+      </p>
+      <p>{location}</p>
+      <div className="social-net">
+        {social.map((item) => (
+          <a href="/" key={item.icon}>
+            <img src={item.icon} alt="social" className="w-8" />
+          </a>
+        ))}
       </div>
     </div>
-  </Link>
+  </div>
 );
 
 SingleDoctor.propTypes = {
   image: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
-  id: PropTypes.number.isRequired,
-  speciality: PropTypes.string.isRequired,
   location: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
 };
