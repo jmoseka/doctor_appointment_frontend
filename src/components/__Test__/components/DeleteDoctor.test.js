e4dc9e7b7a21a4fcdbd3a986256ba24a3a6e729d
jmoseka <jamylam3@gmail.com>
2022-09-28 20:09:32 +0300

    Import redux store

--- src/components/__Test__/components/DeleteDoctor.test.js
+++ src/components/__Test__/components/DeleteDoctor.test.js
@@ -2,3 +2,5 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
+import store from '../../../redux/store';
+import DeleteDoctor from '../../delete_doctor/DeleteDoctor';
