8a51023492ea8f2f75ab66a8b8c295e1459a5dd7
jmoseka <jamylam3@gmail.com>
2022-09-28 20:01:40 +0300

    Add describe function

---
+++ src/components/__Test__/components/Doctors.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
+import store from '../../../redux/store';
+import Doctors from '../../Doctors';
+
+describe('DoctorList', () => {
+  
+});
