6f836fe98d945de6007f5e57d53ab6b39a2a5e73
mandela.t <manderat206@gmail.com>
2022-09-29 08:53:03 +0200

    Add the links from heroku

--- src/config/axios.js
+++ src/config/axios.js
@@ -3,6 +3,6 @@ import axiosInstance from 'axios';
 const { jwt } = JSON.parse(localStorage.getItem('user')) || '';
 
 export default axiosInstance.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: 'https://hidden-citadel-49495.herokuapp.com',
   headers: { Authorization: jwt },
 });
